b083a8c27f99fffbe26b6c4095815a935ed3ac65
Howard-Cui <89081041+Howard-Cui@users.noreply.github.com>
2023-09-14 20:08:24 +1000

    feat: integrate all the design system variables into different scss file and classes, and import into _app.tsx

--- src/pages/font.tsx
+++ src/pages/font.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import classes from "./font.module.scss";
 
 export default function fome() {
   return (
@@ -10,21 +9,21 @@ export default function fome() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={classes.test}>
-        <h1 className={classes.h1_bold}>H1 Headline / Bold</h1>
-        <h1 className={classes.h1_regular}>H1 Headline / Regular</h1>
-        <p className={classes.subtitle_bold}>subtitle / Bold</p>
-        <p className={classes.subtitle_regular}>subtitle / Regular</p>
-        <h2>H2 headline</h2>
-        <h3>H3 headline</h3>
-        <h4>H4 headline</h4>
-        <h5>h5 headline</h5>
-        <p className={classes.main_bold}>Main / Bold</p>
-        <p className={classes.main_regular}>Main / Regular</p>
-        <p className={classes.menu}>MENU</p>
-        <p className={classes.additional}>Additional</p>
-        <p className={classes.small_bold}>Small / Bold</p>
-        <p className={classes.small_regular}>Small / Regular</p>
+      <main>
+        <h1 className="font_h1_headline_bold">H1 Headline / Bold</h1>
+        <h1 className="font_h1_headline_regular">H1 Headline / Regular</h1>
+        <p className="font_subtitle_bold">subtitle / Bold</p>
+        <p className="font_subtitle_regular">subtitle / Regular</p>
+        <h2 className="font_h2_headline">H2 headline</h2>
+        <h3 className="font_h3_headline">H3 headline</h3>
+        <h4 className="font_h4_headline">H4 headline</h4>
+        <h5 className="font_h5_headline">h5 headline</h5>
+        <p className="font_main_bold">Main / Bold</p>
+        <p className="font_main_regular">Main / Regular</p>
+        <p className="font_menu">MENU</p>
+        <p className="font_additional">Additional</p>
+        <p className="font_small_bold">Small / Bold</p>
+        <p className="font_small_regular">Small / Regular</p>
       </main>
     </>
   );
