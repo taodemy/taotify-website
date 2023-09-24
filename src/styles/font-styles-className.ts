907348c2dd411ec36a7157e0a8abed413edcac50
Howard-Cui <89081041+Howard-Cui@users.noreply.github.com>
2023-09-24 20:31:26 +1000

    feat: add the font classname to design system

---
+++ src/styles/font-styles-className.ts
@@ -0,0 +1,25 @@
+import { Montserrat, Raleway } from "next/font/google";
+
+const montserrat = Montserrat({subsets: ["latin"]});
+const raleway = Raleway({subsets: ["latin"]});
+
+const fontClasses = {
+  h1_headline_bold: montserrat.className + " font_h1_headline_bold ",
+  h1_headline_regular: montserrat.className + " font_h1_headline_regular ",
+  subtitle_bold: raleway.className + " font_subtitle_bold ",
+  subtitle_regular: raleway.className + " font_subtitle_regular ",
+  h2_headline: montserrat.className + " font_h2_headline ",
+  h3_headline: montserrat.className + " font_h3_headline ",
+  h4_headline: montserrat.className + " font_h4_headline ",
+  h5_headline: montserrat.className + " font_h5_headline ",
+  main_bold: raleway.className + " font_main_bold ",
+  main_regular: raleway.className + " font_main_regular ",
+  menu: montserrat.className + " font_menu ",
+  additional: montserrat.className + " font_additional ",
+  small_bold: raleway.className + " font_small_bold ",
+  small_regular: raleway.className + " font_small_regular ",
+  /**font sytle other than design systme */
+  font_navigator_logo: raleway.className + " font_navigator_logo ",
+};
+
+export default fontClasses;
