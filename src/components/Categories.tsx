a0d9135d19f93e3f0112f849c5cc04d832e14a75
HjsCS <127371829+HjsCS@users.noreply.github.com>
2023-09-25 12:42:40 +1000

    fix: unify naming specificatoin

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -6,55 +6,55 @@ import JazzIcon from '../assets/JazzIcon.svg';
 
 const Categories = () => {
   return (
-    <section className='Categories'>
+    <section className='categories'>
         {/* title */}
-        <div className="Categories--title">
+        <div className="categories--title">
             <h2 className="font_h2_headline primary-colored-text">Explore </h2>
             <h2 className="font_h2_headline dark-colored-text">By Category</h2>
         </div>
 
         {/* categories */}
-        <div className="Categories--cards">
+        <div className="categories--cards">
             {/* pop category */}
-            <div className="Categories--card Categories--cards-popcard">
+            <div className="categories--card categories--cards-popCard">
                 <PopIcon />
-                <div className="Categories--cards-content Categories--cards-popcard-content">
+                <div className="categories--cards-content categories--cards-popCard-content">
                     <h3 className="font_h3_headline dark-colored-text">Pop</h3>
                     <p className="font_main_regular dark-colored-text">Duis nec sodales nibh. Proin lectus tortor, rutrum vel vulputate vitae</p>
                 </div>
             </div>
 
             {/* folk category */}
-            <div className="Categories--card Categories--cards-folkcard">
+            <div className="categories--card categories--cards-folkCard">
                 <FolkIcon />
-                <div className="Categories--cards-content Categories--cards-folkcard-content">
+                <div className="categories--cards-content categories--cards-folkCard-content">
                     <h3 className="font_h3_headline dark-colored-text">Folk</h3>
                     <p className="font_main_regular dark-colored-text">Duis nec sodales nibh. Proin lectus tortor, rutrum vel vulputate vitae</p>
                 </div>
             </div>
 
             {/* instrumental category */}
-            <div className="Categories--card Categories--cards-instrumentalcard">
+            <div className="categories--card categories--cards-instrumentalCard">
                 <InstrumentalIcon />
-                <div className="Categories--cards-content Categories--cards-instrumentalcard-content">
+                <div className="categories--cards-content categories--cards-instrumentalCard-content">
                     <h3 className="font_h3_headline light-colored-text">Instrumental</h3>
                     <p className="font_main_regular light-colored-text">Duis nec sodales nibh. Proin lectus tortor, rutrum vel vulputate vitae</p>
                 </div>
             </div>
 
             {/* rock category */}
-            <div className="Categories--card Categories--cards-rockcard">
+            <div className="categories--card categories--cards-rockCard">
                 <RockIcon />
-                <div className="Categories--cards-content Categories--cards-rockcard-content">
+                <div className="categories--cards-content categories--cards-rockCard-content">
                     <h3 className="font_h3_headline dark-colored-text">Rock</h3>
                     <p className="font_main_regular dark-colored-text">Duis nec sodales nibh. Proin lectus tortor, rutrum vel vulputate vitae</p>
                 </div>
             </div>
 
             {/* jazz category */}
-            <div className="Categories--card Categories--cards-jazzcard">
+            <div className="categories--card categories--cards-jazzCard">
                 <JazzIcon />
-                <div className="Categories--cards-content Categories--cards-jazzcard-content">
+                <div className="categories--cards-content categories--cards-jazzCard-content">
                     <h3 className="font_h3_headline dark-colored-text">Jazz</h3>
                     <p className="font_main_regular dark-colored-text">Duis nec sodales nibh. Proin lectus tortor, rutrum vel vulputate vitae</p>
                 </div>
