9bc5745366375315b3cbea392610d7c2fba0d854
Kaifeng-Sun <skf951130@gmail.com>
2023-10-17 23:27:02 +1100

    fix: change quote

--- src/components/Testimonial/index.tsx
+++ src/components/Testimonial/index.tsx
@@ -27,7 +27,7 @@ function index() {
                 <p className={fontClasses.font_testimonial_card_text}>
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the
-                  industry's standard dummy text ever since the 1500s,
+                  industry&apos;s standard dummy text ever since the 1500s,
                   when an unknown printer took a galley of type and
                   scrambled it to make a type specimen book.
                 </p>
@@ -81,7 +81,7 @@ function index() {
                 <p className={fontClasses.font_testimonial_card_text}>
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the
-                  industry's standard dummy text ever since the 1500s,
+                  industry&apos;s standard dummy text ever since the 1500s,
                   when an unknown printer took a galley of type and
                   scrambled it to make a type specimen book.
                 </p>
@@ -135,7 +135,7 @@ function index() {
                 <p className={fontClasses.font_testimonial_card_text}>
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the
-                  industry's standard dummy text ever since the 1500s,
+                  industry&apos;s standard dummy text ever since the 1500s,
                   when an unknown printer took a galley of type and
                   scrambled it to make a type specimen book.
                 </p>
